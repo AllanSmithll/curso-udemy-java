20d0da4ff3335fff5f2fbbb264032d49aa153038
allansmithll <allanammancio2004@gmail.com>
2023-01-09 22:16:44 -0300

    Começo do curso com Java

---
+++ secao03-Primeiro_código_java/Main.java
@@ -0,0 +1,10 @@
+ // Main é a classe padrão
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá Mundo! Aqui quem vos digitais é o Allan Amâncio.");
+
+	}
+	
+}
