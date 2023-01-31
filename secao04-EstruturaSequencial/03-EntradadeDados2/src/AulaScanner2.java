0dbb8bbad7db0e2e933afb3cda426feb5ddd0e95
allansmithll <allanammancio2004@gmail.com>
2023-01-30 22:53:54 -0300

    Scanner: parte 2 com Scanner

---
+++ secao04-EstruturaSequencial/03-EntradadeDados2/src/AulaScanner2.java
@@ -0,0 +1,24 @@
+// 30/01/2023
+import java.util.Scanner;
+
+public class AulaScanner2 {
+    public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite alguma frase:");
+		
+		sc.nextLine(); // O nextLine consome um espaço vazio, por isso estou deixando aqui, no caso de haver um string vazio
+		String s1 = sc.nextLine(); // nextLine aceita dados até a quebra de linha
+		String s2 = sc.nextLine(); // Pode ser um string vazio
+		String s3 = sc.nextLine();
+		
+		System.out.println("DADOS DIGITADOS:");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+		sc.close();
+	}
+
+}
