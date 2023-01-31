e22ac0879a8777a23de740c68b34f0b4df48a813
allansmithll <allanammancio2004@gmail.com>
2023-01-30 22:09:21 -0300

    Scanner: finalizando entrada de dados

--- secao04-EstruturaSequencial/02-Entradadedados/src/AulaScanner.java
+++ secao04-EstruturaSequencial/02-Entradadedados/src/AulaScanner.java
@@ -16,39 +16,32 @@ public class AulaScanner {
 		// Trabalhando com strings
 		System.out.print("Digite alguma frase: ");
 		x = sc.nextLine();
-		System.out.println("Você digitou: " + x);
+		System.out.println("Você digitou: " + x); // Concatenação
 
 		// Trabalhando com inteiros
 		System.out.print("Agora digite algum número inteiro: ");
 		y = sc.nextInt();
 		System.out.print("Você digitou: ");
-		System.out.println(y);
+		System.out.println(y); // Devolvendo a variavel na mesma linha
 
 		// Trabalhando com double
 		System.out.print("Agora um Double: "); // Se o estiver programado para o Locale BR, só é aceito , com separador de casas decimais
 		z = sc.nextDouble();
-		System.out.printf("Você digitou: %.2f%n", z);
+		System.out.printf("Você digitou: %.2f%n", z); // Formatando em duas cas flutuantes
 		
 		// Trabalhando com caracteres
 		System.out.print("Digite qualquer caracter: ");
-		zx = sc.next().charAt(0);
+		zx = sc.next().charAt(0); // Qualquer tecla e frase, mas apenas o primeiro caractere
 		System.out.println("Pegamos apenas o primeiro caracter hehehe: " + zx);
 		
 		// Trabalhando com vários tipos
-		System.out.println("Digite uma string, um inteiro e um double por linha:");
-		x = sc.next();
-		y = sc.nextInt();
-		z = sc.nextDouble();
+		System.out.println("Digite uma string, um inteiro e um double por linha:"); // Tudo na mesma linha
+		x = sc.next(); // string
+		y = sc.nextInt(); // Inteiro
+		z = sc.nextDouble(); // Double
 		System.out.print("Você digitou: " + x + ", " + y + ", " + z + ".");
-
-		/*  Versão escrevendo tudo na mesma linha  */
-		// System.out.print("Digite uma string, um inteiro e um double por linha: ");
-		// x = sc.next();
-		// y = sc.nextInt();
-		// z = sc.nextDouble();
-		// System.out.print("Você digitou: " + x + ", " + y + ", " + z + ".");
 		
-		sc.close();
+		sc.close(); // Descarta o objeto do tipo Scanner
 	}
 
 }
