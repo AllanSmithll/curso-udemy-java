d9de68cdeb5fffd1de3ccd8c90d444ec10aef645
allansmithll <allanammancio2004@gmail.com>
2023-02-15 08:57:47 -0300

    Finalizando distribuição de funções

--- secao08-Introducao_POO/2-Triangulo_Problema_com_POO/src/Program.java
+++ secao08-Introducao_POO/2-Triangulo_Problema_com_POO/src/Program.java
@@ -25,11 +25,8 @@ public class Program {
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 		
-		double p = (x.a + x.b + x.c) / 2;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-		
-		double q = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(q * (q - y.a) * (q - y.b) * (q - y.c));
+		double areaX = x.area();
+		double areaY = y.area();
 		
 		System.out.printf("Triangle X area: %.3f%n", areaX);
 		System.out.printf("Triangle Y area: %.3f%n", areaY);
